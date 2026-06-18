6155e5928a58f4e5b469cf2d5e02ff4a749aba9a
Breno-ds-263 <Breno.ds263@gmail.com>
2026-06-18 16:15:44 -0300

    Feat: desconto

--- sistema.py
+++ sistema.py
@@ -1,3 +1,9 @@
 def precoTotal(quantidade, preco):
     total = quantidade * preco
-    return total;
+    desconto = 0
+
+    if total > 100:
+        desconto = total * 0.10
+
+    return total - desconto;
+
